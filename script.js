684e45880e387885f4e5e8ff84c90bfdc799e18b
Md. Adib Chowdhury <mdadib300@gmail.com>
2023-02-28 15:48:50 +0600

    variables diclared

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// print output
 console.log('kire bhai')
+
+// declaring variable
+
+// string
+var name = 'Adib Chowdhury'; 
+// numeric
+const birthYear = 2005;
+let age = 17;
+// boolean
+const isDead = false;
